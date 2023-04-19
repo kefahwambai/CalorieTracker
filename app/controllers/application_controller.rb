15d51c84b5f3fbd9db59e19998ceeed43725abea
KefahWambai <kefah.ngwei@student.moringaschool.com>
2023-04-19 09:05:45 +0300

    corrected def current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,9 @@ class ApplicationController < ActionController::API
   
     # before_action :authorized_user
   
-    # def current_user
-    #   @current_user ||= User.find_by(id: session[:user_id])
-    # end
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
   
     # def authorized_user
     #   render json: { error: 'You need to log in first' }, status: :unauthorized unless current_user
